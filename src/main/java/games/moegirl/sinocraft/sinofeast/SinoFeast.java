4d8bec7af3ad0015c33f7a98e3e5a6b40e232c03
秋雨落 <i@rain.cx>
2022-01-29 16:05:13 +0800

    feat(mod): Init mod structure.
    Signed-off-by: 秋雨落 <i@rain.cx>

---
+++ src/main/java/games/moegirl/sinocraft/sinofeast/SinoFeast.java
@@ -0,0 +1,12 @@
+package games.moegirl.sinocraft.sinofeast;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(SinoFeast.MODID)
+public class SinoFeast {
+    public static final String MODID = "sinofeast";
+
+    public SinoFeast() {
+        
+    }
+}
